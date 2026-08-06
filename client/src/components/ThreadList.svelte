8320f230cefdc0c03a5f4c49f52a5a050f2a8a2e
Haruka-Izumi18 <haruka.izumi18@gmail.com>
2026-08-06 11:39:14 +0200

    page forum

--- client/src/components/ThreadList.svelte
+++ client/src/components/ThreadList.svelte
@@ -3,21 +3,28 @@
   import { onMount } from "svelte";
   import { api } from "../service/api.service.js";
 
-  /** @type {Array<{id: number, title: string, body: string, user?: {name: string}}>} */
+  /** @type {Array<{id: number, title: string, body: string, created_at: string,
+  updated_at: string, user?: {name: string}}>} */
   let threads = $state([]);
   let loading = $state(true);
   let error = $state("");
 
   onMount(async () => {
+    console.log("onMount");
     try {
         const data = await api.allThread();
+        console.log("data =", data);
+        console.log("threads =", data?.threads);
         threads = data.threads;
     }  catch (err) {
             error = "Impossible de récupérer le sujet."
         } finally {
             loading = false;
         }
-  })
+  });
+  function formatDate(date) {
+    return new Date(date).toLocaleString("fr-FR");
+  }
 </script>
 
 <div class="thread-list">
@@ -30,9 +37,14 @@
   {:else}
     {#each threads as thread}
       <a href="/thread/{thread.id}" class="thread-card">
+      <div class="thread-header">
         <h3>{thread.title}</h3>
-        <p class="body">{thread.body}</p>
         <span class="author">{thread.user?.name}</span>
+        <p class="date">
+  {formatDate(thread.created_at)}
+</p>
+       
+</div>
       </a>
     {/each}
   {/if}
@@ -42,12 +54,14 @@
   .thread-list {
     display: flex;
     flex-direction: column;
-    gap: 0.8rem;
+    gap: 1rem;
+    margin-top: 1rem;
   }
 
   .thread-card {
     display: block;
     text-decoration: none;
+    flex: row;
     color: inherit;
     border-radius: 8px;
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.332);
@@ -59,22 +73,26 @@
     transform: scale(1.02);
   }
 
+  .thread-card .thread-header{
+    display: flex;
+    align-items: baseline;
+    gap: 1rem;
+  }
+  
   .thread-card h3 {
     margin: 0 0 0.4rem 0;
+    flex: 1;
     color: var(--color-text);
   }
 
-  .thread-card .body {
-    margin: 0 0 0.4rem 0;
-    color: #666;
-    font-size: 0.9rem;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-  }
-
   .thread-card .author {
-    font-size: 0.8rem;
+    font-size: 1rem;
     color: #999;
   }
+
+  .thread-card .date {
+  font-size: 0.8rem;
+  color: #999;
+  margin-top: 0.5rem;
+}
 </style>
